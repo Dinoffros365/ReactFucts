165a50e4f8c1229e39ad79368961037e07b6b2c3
Yaroslav <halutaa17@gmail.com>
2024-03-10 10:55:32 +0300

    new file

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <img className="nav__img" src="./src/assets/react.svg"/>
+            <h3 className="nav__h3">ReactFucts</h3>
+            <h4 className="nav__h4">React Course - Project 1</h4>
+        </nav>
+    )
+}
